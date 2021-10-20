7ef6243f77f87d5610aeff7922969de71fc3aeeb
ckodser <arshia.soltani2@gmail.com>
2021-10-20 17:22:41 +0330

    add Net to model dictionary

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from models.resnet import ResNet18, ResNet50, ResNet101, WideResNet50_2, WideResNet101_2
 from models.resnet_cifar import cResNet18, cResNet50, cResNet101
-from models.frankle import FC, Conv2, Conv4, Conv6, Conv4Wide, Conv8, Conv6Wide
+from models.frankle import FC, Conv2, Conv4, Conv6, Conv4Wide, Conv8, Conv6Wide, Net
 
 __all__ = [
     "ResNet18",
@@ -11,6 +11,7 @@ __all__ = [
     "WideResNet50_2",
     "WideResNet101_2",
     "FC",
+    "Net",
     "Conv2",
     "Conv4",
     "Conv6",
