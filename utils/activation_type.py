2673a0f9b602d2a5155ff15a2180ef411b9489b5
ckodser <arshia.soltani2@gmail.com>
2021-10-25 16:56:02 +0330

    debug MaxMin

--- utils/activation_type.py
+++ utils/activation_type.py
@@ -1,44 +1,18 @@
 import torch.nn as nn
 import torch
 
+
 class MaxMin(nn.Module):
     # code from https://github.com/cemanil/LNets/blob/master/lnets/models/activations/maxout.py
     def __init__(self):
         super(MaxMin, self).__init__()
 
     def forward(self, x):
-
-        maxes = maxout(x, self.num_units, self.axis)
-        mins = minout(x, self.num_units, self.axis)
+        y = torch.reshape(x, (x.shape[0], x.shape[1] / 2, 2, x.shape[2], x.shape[3]))
+        maxes = torch.max(y, 2)
+        mins = torch.min(y, 2)
         maxmin = torch.cat((maxes, mins), dim=1)
         return maxmin
 
     def extra_repr(self):
         return 'num_units: {}'.format(self.num_units)
-
-
-def process_maxmin_size(x, num_units, axis=-1):
-    size = list(x.size())
-    num_channels = size[axis]
-
-    if num_channels % num_units:
-        raise ValueError('number of features({}) is not a '
-                         'multiple of num_units({})'.format(num_channels, num_units))
-    size[axis] = -1
-    if axis == -1:
-        size += [num_channels // num_units]
-    else:
-        size.insert(axis + 1, num_channels // num_units)
-    return size
-
-
-def maxout(x, num_units, axis=-1):
-    size = process_maxmin_size(x, num_units, axis)
-    sort_dim = axis if axis == -1 else axis + 1
-    return torch.max(x.view(*size), sort_dim)[0]
-
-
-def minout(x, num_units, axis=-1):
-    size = process_maxmin_size(x, num_units, axis)
-    sort_dim = axis if axis == -1 else axis + 1
-    return torch.min(x.view(*size), sort_dim)[0]
